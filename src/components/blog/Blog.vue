b9eacf3a7d355e949742c3c7d992b7011bece47f
woshiliyu <513422479@qq.com>
2019-04-13 16:54:47 +0800

    博客系统第一次提交

---
+++ src/components/blog/Blog.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="blog">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>卡片名称</span>
+        </div>
+        <div v-for="o in 4" :key="o" class="text item">
+          {{'列表内容 ' + o }}
+        </div>
+      </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'blog',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .text {
+    font-size: 14px;
+  }
+  .item {
+    margin-bottom: 18px;
+    height:20px;
+  }
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+  .box-card {
+    width: 480px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+//组件
+import HelloWorld from './components/HelloWorld.vue'
+import WangEditor from './components/WangEditor.vue'
+import Blog       from './components/blog/Blog.vue'
+//elementui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+//axios请求
+import axios from 'axios'
+window.axios = axios
+//路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+    {path: '/',component: HelloWorld},
+    {path: '/editor',component: WangEditor},
+    {path: '/blog',component: Blog},
+    {path: '*',redirect: '/'} //防输错或者没找到页面路由错误，跳转到首页
+]
+const router = new VueRouter ({
+    routes,
+    mode: 'history'
+})
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
